7d63439a1572c9062b89a528c847474f0d33bf6f
aysonsteven <aysonsteven@gmail.com>
2016-10-10 17:44:24 +0800

    UPDATE

--- src/providers/questions-list.ts
+++ src/providers/questions-list.ts
@@ -7,7 +7,18 @@ export const questionsList = [
   { question: 'It consists of a command line client that interacts with a remote registry. It allows users to consume and distribute JavaScript modules that are available on the registry.', choices: ['AJAX', 'npm', 'Babel', 'ReactJS'], answer: 1 },
   { question: 'It is the most popular JavaScript library in use today, with installation on 65% of the top 10 million highest-trafficked sites on the Web.', choices: ['Ajax', 'PHP', 'JS', 'jQuery'], answer: 3 },
   { question: 'It is the use of the XMLHttpRequest object to communicate with server-side scripts. It can send as well as receive information in a variety of formats.', choices: ['Ajax', 'TypeScript', 'JS', 'Function JS'], answer: 0 },
-  { question: 'It is a lightweight data-interchange format. It is easy for humans to read and write. It is easy for machines to parse and generate.', choices: ['EcmaScript', 'JavaScript', 'JSON', 'Function JS'], answer: 2 }
+  { question: 'It is a lightweight data-interchange format. It is easy for humans to read and write. It is easy for machines to parse and generate.', choices: ['EcmaScript', 'JavaScript', 'JSON', 'Function JS'], answer: 2 },
+  { question: 'Is a middle-level programming language developed by Bjarne Stroustrup starting in 1979 at Bell Labs.', choices: ['C#','Ruby','C++','Java'], answer: 2 },
+  { question: 'Is a Unix-like and mostly POSIX-compliant computer operating system (OS) assembled under the model of free and open-source software development and distribution.', choices: ['Linux','MacOSX','Debian','Windows'], answer: 0 },
+  { question: 'He created the C programming language and, with long-time colleague Ken Thompson, the Unix operating system.', choices: ['Steve Wozniak','Dennis Ritchie','Bill Gates','Steve Jobs'], answer: 1 },
+  { question: 'Is an open-source[2] server-side web application framework designed for web development to produce dynamic web pages. It was developed by Microsoft to allow programmers to build dynamic web sites, web applications and web services.', choices: ['PHP','JSP','Perl','ASP'], answer: 0 },
+  { question: 'Is a technology that helps software developers create dynamically generated web pages based on HTML, XML, or other document types. Released in 1999 by Sun Microsystems.', choices: ['PHP','JSP','ASP','Ruby'], answer: 1 },
+  { question: 'Is a software pattern that devides a given software app into 3 interconnected parts to seperate internal representations', choices: ['MVC','AngularJS','IDE','ReactJS'], answer: 0 },
+  { question: 'Is an evolution of the MVC pattern used for most web applications today. It came about as an answer to the salability problems apparent within applications which used MVC.?', choices: ['Asynchronous','HMVC','Component','OOP'], answer: 1 },
+  { question: 'Is a general-purpose, concurrent, functional programming language. It is also a garbage-collected runtime system.', choices: ['Erlang','Java','Gulp','C#'], answer: 0 },
+  { question: 'Is the most mature, stable, and powerful professional grade CSS extension language in the world.', choices: ['Bootstrap','Sass','Stylesheet','MD'], answer: 1 },
+  { question: 'Is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first web sites.?', choices: ['ionicons','Sass','CSS3','Bootstrap'], answer: 3 },
+  { question: 'Is a forked project of GNOME2. It provides an intuitive and attractive desktop environment using traditional metaphors for Linux and other Unix-like operating systems.', choices: ['MATE','GNOME3','GNOME2','distro'], answer: 0 }
   ];
         
     
